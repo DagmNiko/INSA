0f92f8bc2f2fb504a087078bc1d0d6c582dd5c96
Dagm Niko <dagmniko79@gmail.com>
2023-05-03 00:14:42 +0300

    ordinary

--- core/consumers.py
+++ core/consumers.py
@@ -22,17 +22,18 @@ class BlogConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-        
+        name = text_data_json["name"]
         # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat_message", "message": message}
+            self.room_group_name, {"type": "chat_message", "message": message, "name": name}
         )
         print(f'Received message from {self.channel_name}: {message}')
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event["message"]
+        name = event["name"]
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps({"message": message, 'name': name}))
         
     
