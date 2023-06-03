2b05b2ede49be9f1d1fd7917ff849204a37bdcc0
Dagm Niko <dagmniko79@gmail.com>
2023-06-03 21:59:09 +0300

    update frontend/welcome.js add animation

--- frontend/welcome.js
+++ frontend/welcome.js
@@ -1,11 +1,25 @@
+const ellipse3 = document.querySelector("#Ellipse3"); 
+const user = document.querySelector("#user"); 
+const password = document.querySelector("#password"); 
+const card = document.querySelector("#card"); 
+const rectangle2 = document.querySelector("#Rectangle2"); 
+
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
-            
-            document.querySelector(".second svg").classList.add('animate')
+            ellipse3.classList.add('Ellipse3Animate');
+            user.classList.add('userAnimate');
+            password.classList.add('passwordAnimate');
+            card.classList.add('cardAnimate');
+            rectangle2.classList.add('Rectangle2Animate');
         }else {
-            document.querySelector(".second svg").classList.remove('animate')
+            ellipse3.classList.remove('Ellipse3Animate');
+            user.classList.remove('userAnimate');
+            password.classList.remove('passwordAnimate');
+            card.classList.remove('cardAnimate');
+            rectangle2.classList.remove('Rectangle2Animate');
         }
     });
 });
